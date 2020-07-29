ca61bab08bc1cae279c8dc7b0e98df7cebbc2882
Matthew Torrence <matt@torrencefamily.net>
2020-07-29 11:45:20 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::marker::PhantomData;
+
+/// An owned value which has type name Name. This type
+/// is unique to this Named, so it can be used to
+/// enforce compile-time coherency
+pub struct Named<T, Name> {
+    inner: T,
+    _phantom: PhantomData<Name>,
+}
+
+/// Create a named value. You probably don't want to use
+/// this, but instead want to use the name!() macro that
+/// calls this.
+///
+/// Safety:
+/// Must make sure Name is not used as the name for any
+/// other value of type Named<T, Name>
+pub unsafe fn name<Name, T>(val: T) -> Named<T, Name> {
+    Named {
+        inner: val,
+        _phantom: PhantomData,
+    }
+}
+
+impl<T, Name> Named<T, Name> {
+    pub fn unname(self) -> T {
+        self.inner
+    }
+
+    pub fn unname_ref(&self) -> &T {
+        &self.inner
+    }
+
+    /// Safety:
+    /// Must uphold whatever invariant the Named protects
+    pub unsafe fn unname_ref_mut(&mut self) -> &mut T {
+        &mut self.inner
+    }
+}
+
+#[macro_export]
+macro_rules! name {
+    ($val:expr) => {{
+        struct Uniquename {};
+
+        unsafe {
+            // Nothing else is named UniqueName because
+            // we just defined it
+            name::<UniqueName, _>($val)
+        }
+    }}
+}
